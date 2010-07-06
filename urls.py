1c29cf3f5af7c5771d8ed248b62ef454f8df6341
Wilson Felipe <wfelipe@gmail.com>
2010-07-05 22:37:56 -0300

    project divided in three parts: - click: responsible by handling the clicks - reports: showing the clicks in a human readable form - heat: site heat map, based on click reports

--- urls.py
+++ urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^hotclick/', include('hotclick.foo.urls')),
+	# Example:
+	# (r'^hotclick/', include('hotclick.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+	# to INSTALLED_APPS to enable admin documentation:
+	# (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	(r'^admin/', include(admin.site.urls)),
+
+	# click
+	(r'^click/' include('hotclick.click.urls')),
+	# reports
+	(r'^reports/' include('hotclick.reports.urls')),
+	# heat
+	(r'^heat/' include('hotclick.heat.urls')),
 )
