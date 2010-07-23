2f83e4c5ff9ed1e91465f833214351d3d8a7055b
Danilo Sousa <danilopopeye@gmail.com>
2010-07-23 19:50:59 -0300

    First version posting some data;

--- docs/test/hotclick.js
+++ docs/test/hotclick.js
@@ -12,6 +12,8 @@
 		init: function(){
 			this.event();
 
+			this.ajax();
+
 			log('HotClick initialized');
 		},
 		event: function(){
@@ -19,14 +21,65 @@
 			document.addEventListener && document.addEventListener('mouseup', this.click, false);
 			document.attachEvent && document.attachEvent('onmouseup', this.click);
 		},
-		click: function(e){
-				// only pay attention on left click
-			if( e.button != 0 ){
-				return;
+		click: function(event){
+			var e = h.fixEvent( event ),
+					opt = {
+						platform: navigator.platform,
+						x: e.clientX,
+						y: e.clientY
+					};
+
+			// ignore right click
+			if( e.which == 3 ){
+				return false;
 			}
 
-			log(e, e.clientX, e.clientY);
-			log(e.originalTarget, e.currentTarget);
+			opt.link = escape( h.link(e) );
+
+			h.req('?'+ [
+				'x='+ opt.x,
+				'y='+ opt.y,
+				'link='+ opt.link
+			].join('&') );
+		},
+		fixEvent: function(event){
+			// Fix target property, if necessary
+			if ( !event.target ) {
+				event.target = event.srcElement || document; // Fixes #1925 where srcElement might not be defined either
+			}
+
+			// check if target is a textnode (safari)
+			if ( event.target.nodeType === 3 ) {
+				event.target = event.target.parentNode;
+			}
+
+			// Add which for click: 1 === left; 2 === middle; 3 === right
+			// Note: button is not normalized, so don't use it
+			if ( !event.which && event.button !== undefined ) {
+				event.which = (event.button & 1 ? 1 : ( event.button & 2 ? 3 : ( event.button & 4 ? 2 : 0 ) ));
+			}
+
+			return event;
+		},
+		link: function(e){
+			return e.target.tagName == 'A'
+				? e.target.href : ''
+		},
+		ajax: function(){
+			if( window.XMLHttpRequest && !window.ActiveXObject ){
+				this.xhr = new window.XMLHttpRequest();
+			} else {
+				try {
+					this.xhr = new window.ActiveXObject("Microsoft.XMLHTTP");
+				} catch(e) {
+					this.xhr = false;
+				}
+			}
+		},
+		req: function(url){
+			var xhr = this.xhr;
+			xhr.open('get', url, false);
+			xhr.send( null );
 		}
 	};
 })();
