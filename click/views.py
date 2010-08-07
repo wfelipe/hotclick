4fb42f5a937eed7bd93632fe850e9e27b5a27e86
Wilson Felipe <wfelipe@gmail.com>
2010-08-07 13:55:11 -0300

    better error handling

--- click/views.py
+++ click/views.py
@@ -1,21 +1,24 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest
 from hotclick.click.models import *
+from django.db.utils import IntegrityError
 
 def process_click(request):
 	click = Click()
 
 	if not 'HTTP_REFERER' in request.META:
-		return HttpResponse('NOT OK - Referer')
+		return HttpResponseBadRequest('NOT OK - Referer')
 	try:
 		click.siteurl = SiteURL.objects.get(url=request.META['HTTP_REFERER'])
 	except SiteURL.DoesNotExist:
-		return HttpResponse('Site does not exist')
+		return HttpResponseNotFound('Site does not exist')
 
-	print request.GET
 	for attr in request.GET:
 		if hasattr(click, attr):
 			setattr(click, attr, request.GET[attr])
 
-	click.save()
+	try:
+		click.save()
+	except IntegrityError as error:
+		return HttpResponseBadRequest("Missing argument: " + error.args[0])
 
 	return HttpResponse('OK')
